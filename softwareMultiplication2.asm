fc530b14d833eef3e97e63abe668f007766f931b
SleepingFox88 <seanloganking@gmail.com>
2019-06-29 03:27:05 -0400

    Multiplication working

---
+++ softwareMultiplication2.asm
@@ -0,0 +1,39 @@
+;software multiplication program
+;arguments are stored in global variables
+;result is returned in register D
+
+	JMP main
+
+;global variables
+varA:
+	DB 0
+varB:
+	DB 0
+
+multiply:
+	MOV C, 0
+	MOV D, 0
+.multiplyLoop:
+	MOV A, [varA]
+	AND A, 1
+	JZ dontAdd	; if 1, add
+	ADD D, [varB]
+dontAdd:
+	MOV A, [varA]	;shift varA right by 1
+	SHR A, 1
+	MOV [varA], A
+
+	MOV A, [varB]	;shift varB left by 1
+	SHL A, 1
+	MOV [varB], A
+
+	INC C		;for loop comparison "for(i=0; i<8; i++)"
+	CMP C, 8
+	JNZ .multiplyLoop
+	RET
+
+main:
+	MOV [varA], 21
+	MOV [varB], 3
+	CALL multiply
+	HLT
